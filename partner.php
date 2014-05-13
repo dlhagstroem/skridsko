73504baf10659448558c929bb4d9ece0a99a95cb
marcni13 <nilssonmarcus_@hotmail.com>
2014-05-13 11:25:44 +0200

    partner

---
+++ partner.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('inc/HTMLTemplate.php');
+include_once('db.php');
+
+$mysqli->set_charset("utf8");
+
+$query = 'SELECT *
+		  FROM partner  
+		  GROUP BY partner.partnerId
+		  ORDER BY partner.partnerId DESC';
+
+ $res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error);
+
+
+$content = <<<END
+
+		<div>
+		while($row = $res->fetch_object()) : 
+	         <a href="<?php echo $->linkUrl ?>"><img src="<?php echo $row->imageUrl ?></a>">
+        </div>
+
+END;
+
+
+echo $header;
+echo $content;
+echo $footer;
+
+
+
+?>
