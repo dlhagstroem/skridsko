67c765574679d668cc30773423982fc7e2d418c5
dlhagstroem <dennis.lh@live.se>
2014-08-14 11:35:53 +0200

    dåså

--- gb-delete.php
+++ gb-delete.php
@@ -1,16 +1,109 @@
 <?php
+/*----------------------
+gb-delete.php
+Deletes chosen post or comment from DB
+----------------------*/
 
-include_once("inc/connstring.php");
+include_once("inc/HTMLTemplate.php");
 
-$postId = isset($_GET['pid']) ? $_GET['pid'] : '';
+if(!isset($_SESSION["username"])) {
+	header("Location: index.php");
+	exit();
+}
 
-$query =<<<END
-DELETE FROM post
-WHERE postId = $postId
+include_once("inc/db.php");
+
+$tablePost = 	"post";
+$tableComment = "comment";
+
+$feedback 	= "";
+$name 		= "";
+$msg 		= "";
+
+$postId = 	isset($_GET['pid']) 	? 	$_GET['pid'] 	: '';
+$commId = 	isset($_GET['cid']) 	? 	$_GET['cid'] 	: '';
+$s = 		isset($_GET['s']) 		? 	$_GET['s'] 		: '';
+
+if($postId == '' && $commId == '') {
+	$content = <<<END
+			<div id="breadcrumbs">
+				<p><a href="gb.php">Guestbook</a> &gt; Delete</p>
+			</div><!-- breadcrumbs -->
+			<div id="container">
+				<p>No post or comment has been chosen. Please try again.</p>
+			</div><!-- container -->
 END;
 
-$res = $mysqli->query($query) or die("You failed miserably.");
+} else if ($s != '') {
+	$query = "";
+	
+	if($postId == "y") {
+		$postId 	= $mysqli->real_escape_string($postId);
+		$type = "post";
+		
+		//------------------------
+			//SQL query
+			$query = <<<END
+			--
+			-- Deletes chosen post
+			--
+			DELETE FROM {$tablePost}
+			WHERE postId = {$postId};
 
-header("Location: gb.php");
+END;
+
+	} else if ($commId != '') {
+		$commId 	= $mysqli->real_escape_string($commId);
+		$type = "comment";
+		
+		//------------------------
+			//SQL query
+			$query = <<<END
+			--
+			-- Deletes chosen comment
+			--
+			DELETE FROM {$tableComment}
+			WHERE commId = {$commId};
+
+END;
+	} 
+	
+	$mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Performs query
+	
+	if($mysqli->affected_rows >= 1) {
+		$feedback = "The {$type} has been removed.";
+	} else {
+		$feedback = "Something went wrong and the {$type} was not removed.";
+	}
+	
+	$mysqli->close();
+	
+	$content = <<<END
+			<div id="breadcrumbs">
+				<p><a href="gb.php">Guestbook</a> &gt; Delete</p>
+			</div><!-- breadcrumbs -->
+			<div id="container">
+				<p>{$feedback}</p>
+				<p><a href="gb.php">Back to guestbook</a></p>
+			</div><!-- container -->
+END;
+} else {
+	$type = ($postId != '') ? "post" : "comment" ;
+
+	$content = <<<END
+			<div id="breadcrumbs">
+				<p><a href="gb.php">Guestbook</a> &gt; Delete</p>
+			</div><!-- breadcrumbs -->
+			<div id="container">
+				<p>Are you sure you want to remove the chosen {$type}?</p>
+				<p><a href="gb-delete.php?pid={$postId}&cid={$commId}&s=y">Yes</a></p>
+			</div><!-- container -->
+	
+END;
+}
+
+echo $header;
+echo $content;
+echo $footer;
 
 ?>
--- gb-edit.php
+++ gb-edit.php
@@ -1,14 +1,23 @@
 <?php
+/*-----------------------------
+gb-edit.php
+Allows editing of chosen post or comment
+-----------------------------*/
 
 include_once("inc/HTMLTemplate.php");
-include_once("inc/db.php");
 
-$tablePost = "post";
+if(!isset($_SESSION["username"])) {
+	header("Location: index.php");
+	exit();
+}
+
+include_once("inc/db.php");
+$tablePost = 	"post";
 $tableComment = "comment";
 
-$feedback = "";
-$name = "";
-$msg = "";
+$feedback 	= "";
+$name 		= "";
+$msg 		= "";
 
 $postId = isset($_GET['pid']) ? $_GET['pid'] : '';
 $commId = isset($_GET['cid']) ? $_GET['cid'] : '';
@@ -16,58 +25,106 @@ $commId = isset($_GET['cid']) ? $_GET['cid'] : '';
 $type = ($postId != '') ? "post" : "comment" ;
 
 if(!empty($_POST)) {
-
-	//edit post or comment in DB!
-
+	
+	$name = 	isset($_POST['name']) ? $_POST['name'] : '';
+	$msg = 		isset($_POST['msg']) ? $_POST['msg'] : '';
+	
+	if($name == '' || $msg == '') {
+		$feedback = "<p class=\"feedback-yellow\">Please fill out all fields.</p>";
+		
+		$content = getFormHTML($type, $postId, $commId, $name, $msg, $feedback);
+			
+	} else {
+	
+		//--------------------------
+		//Prevents SQL injections
+		$name 	= utf8_encode($mysqli->real_escape_string($name));
+		$msg 	= utf8_encode($mysqli->real_escape_string($msg));
+		
+		if($type == "post") {
+			$query = <<<END
+			--
+			-- Changes chosen post in DB
+			--
+			UPDATE {$tablePost}
+			SET postName = '{$name}', postMessage = '{$msg}'
+			WHERE postId = {$postId};
+END;
+			
+		} else {
+			$query = <<<END
+			--
+			-- Changes chosen comment in DB
+			--
+			UPDATE {$tableComment}
+			SET commName = '{$name}', commMessage = '{$msg}'
+			WHERE commId = {$commId};
+END;
+		}
+	
+		$mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Performs query
+		
+		if($mysqli->affected_rows >= 1) {
+			$feedback = "The {$type} has been changed.";
+		} else {
+			$feedback = "Something went wrong and the {$type} was not changed.";
+		}
+		
+		$mysqli->close();
+		
+		$content = <<<END
+				<div id="container">
+					<p>{$feedback}</p>
+					<p><a href="gb.php">Back to guestbook</a></p>
+				</div><!-- container -->
+END;
+	
+	}
+	
 } else {
 
 	if($type == "post") {
-
-		$query =<<<END
+		$query = <<<END
 		--
-		--Gets chosen post from DB
+		-- Gets chosen post from DB
 		--
 		SELECT postId, postName, postMessage, postTimestamp, adminId
-		FROM {tablePost}
+		FROM {$tablePost}
 		WHERE postId = {$postId};
-END;
 
+END;
 	} else {
-
-	$query = <<<END
-	--
-	-- Gets chosen comment from DB
-	--
-	SELECT commId, commName, commMessage, commTimestamp, adminId
-	FROM {$tableComment}
-	WHERE commId = {$commId};
+		$query = <<<END
+		--
+		-- Gets chosen comment from DB
+		--
+		SELECT  commId, commName, commMessage, commTimestamp, adminId
+		FROM {$tableComment}
+		WHERE commId = {$commId};
 
 END;
-
 	}
 
-	$res =$mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Perf
-
+	$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . " : " . $mysqli->error); //Performs query
+		
 	if($res->num_rows < 1) {
 		$content = <<<END
 		<div id="container">
 			<p>The {$type} you chose cannot be found. Please try again.</p>
-			<p><a href="gb.php">Gästbok</a></p>
+			<p><a href="gb.php">Guestbook</a></p>
 		</div><!-- container -->
 END;
 
 	} else {
 		$row = $res->fetch_object(); //Gets result from DB
-
-		$name = ($type == "post") ? $row->postName : $row->commName;
-		$msg = ($type == "post") ? $row->postMessage : $row->commMessage;
-
-		$name = utf8_decode($name);
-		$msg = utf8_decode($msg);
-
+		
+		$name = ($type == "post") ? $row->postName 		: $row->commName;
+		$msg = 	($type == "post") ? $row->postMessage 	: $row->commMessage;
+		
+		$name 	= utf8_decode($name);
+		$msg 	= utf8_decode($msg);
+		
 		$content = getFormHTML($type, $postId, $commId, $name, $msg, $feedback);
-
-
 	}
 }
 
@@ -76,25 +133,26 @@ echo $content;
 echo $footer;
 
 function getFormHTML($type, $postId, $commId, $name, $msg, $feedback) {
-	$name = htmlspecialchars($name);
-	$msg = htmlspecialchars($msg);
-
+	$name 	= htmlspecialchars($name);
+	$msg 	= htmlspecialchars($msg);
+	
 	return <<<END
-		<div id="breadcrumbs">
-			<p><a href="gb.php">Gästbok</a> &gt; Edit</p>
-		</div><!-- breadcrumbs -->
-
-		<div id="container">
-			<h2>Edit the chosen {$type}</h2>
-			{$feedback}
-			<form action="gb-edit.php?pid={$postId}&cid={$commID}" method="post">
-				<label for ="name">Name:</label>
-				<input type="text" id="name" name="name" value="{$name}" />
-				<label for="msg">Message:</label>
-				<textarea id ="msg" name="msg">{$msg}</textarea>
-				<input type ="submit" value="Save changes" />
-			</form>
-		</div><!-- container -->
+			<div id="breadcrumbs">
+				<p><a href="gb.php">Guestbook</a> &gt; Edit</p>
+			</div><!-- breadcrumbs -->
+			
+			<div id="container"> 
+				<h2>Edit the chosen {$type}</h2>
+				{$feedback}
+				<form action="gb-edit.php?pid={$postId}&cid={$commId}" method="post">
+					<label for="name">Name:</label>
+					<input type="text" id="name" name="name" value="{$name}" />
+					<label for="msg">Message:</label>
+					<textarea id="msg" name="msg">{$msg}</textarea>
+					<input type="submit" value="Save changes" />
+				</form>
+			</div><!-- container -->
+	
 END;
 }
 
